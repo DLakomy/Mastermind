9ade98c3cda6bab5f7805649f96704ce53b741f9
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-07-09 11:58:43 +0200

    Add and apply Scalafmt

--- src/main/scala/mastermind/MasterMind.scala
+++ src/main/scala/mastermind/MasterMind.scala
@@ -2,28 +2,27 @@
 
 package mastermind
 
-import scala.annotation.tailrec
-     , scala.io.StdIn.readLine
+import scala.annotation.tailrec, scala.io.StdIn.readLine
 
 package object defaults {
   val defMaxTurnNumber = 12 // how many times one can guess?
-  val defCodeLength = 5 // how long is the code?
-  val defMaxDigit = 6 // the code consists of numbers from 1 to defMaxDigit
+  val defCodeLength    = 5  // how long is the code?
+  val defMaxDigit      = 6  // the code consists of numbers from 1 to defMaxDigit
 
-  val greeting = "You are playing MASTERMIND\n"+
-                 "Guess the code. Read the rules somewhere else.\n"+
-                 "The author was lazy, thus you won't find'em here.\n"+
-                 "\n"+
-                 "BTW. You can type three digits as arguments to change\n"+
-                 "the game parameters. Sorry for not providing better input option.\n"+
-                 "\n"+
-                 "When you guess the code, separate the numbers using space."
+  val greeting = "You are playing MASTERMIND\n" +
+    "Guess the code. Read the rules somewhere else.\n" +
+    "The author was lazy, thus you won't find'em here.\n" +
+    "\n" +
+    "BTW. You can type three digits as arguments to change\n" +
+    "the game parameters. Sorry for not providing better input option.\n" +
+    "\n" +
+    "When you guess the code, separate the numbers using space."
 
-  val invalidParamsWarning = "WARNING! The parameters given were invalid. "+
-                             "Default values have been assumed."
+  val invalidParamsWarning = "WARNING! The parameters given were invalid. " +
+    "Default values have been assumed."
 
   val loseMessage = "No more chances. You lose."
-  val wonMessage = "You won. Congratulations."
+  val wonMessage  = "You won. Congratulations."
 
   type CodeType = Seq[Int]
 }
@@ -32,33 +31,34 @@ object MasterMind {
 
   type Code = defaults.CodeType
 
-  def generateCode( codeLength : Int, maxDigit : Int ) : Code = {
+  def generateCode(codeLength: Int, maxDigit: Int): Code = {
 
     val randomGen = new util.Random(System.currentTimeMillis)
 
     @tailrec
-    def generationLoop( code : Code = Nil ) : Code = {
+    def generationLoop(code: Code = Nil): Code = {
       if (code.length < codeLength) {
-        generationLoop(code :+ randomGen.nextInt(maxDigit)+1) // +1 so as not to include 0
+        generationLoop(code :+ randomGen.nextInt(maxDigit) + 1) // +1 so as not to include 0
       } else code
     }
 
     generationLoop()
   }
 
-  def checkGuess( theCode : Code, guess : Code ) : Map[String,Int] = {
+  def checkGuess(theCode: Code, guess: Code): Map[String, Int] = {
 
-    implicit class Groupable ( x: Code ) extends AnyRef {
-      def groupDigits: Map[Int,Int] = x.groupBy(identity).mapValues(_.length).toMap
+    implicit class Groupable(x: Code) extends AnyRef {
+      def groupDigits: Map[Int, Int] = x.groupBy(identity).mapValues(_.length).toMap
     }
 
     // maps digits in the guess to the number
     // of their occurencies in the correct place
     val correctDigits =
-      (for { x <- theCode.zipWithIndex
-             y <- guess.zipWithIndex
-             if x._2 == y._2 && x._1 == y._1 // _1 stands for value, _2 for index
-       } yield x._1).groupDigits
+      (for {
+        x <- theCode.zipWithIndex
+        y <- guess.zipWithIndex
+        if x._2 == y._2 && x._1 == y._1 // _1 stands for value, _2 for index
+      } yield x._1).groupDigits
 
     // maps digits in the guess to the number of their occurencies,
     // only misplaced ones without the place to fit them
@@ -66,24 +66,23 @@ object MasterMind {
     // there is a '2' in its correct place, so there is no place to move it (unlike '4')
     // to sum up, this value indicates misplaced numbers that could be moved to its place
     val groupedCodeRest = // I group here the digits in the code which are not guessed correctly
-      (for { x <- theCode.zipWithIndex
-             y <- guess.zipWithIndex
-             if x._2 == y._2 && x._1 != y._1 // _1 stands for value, _2 for index
-       } yield x._1).groupDigits
+      (for {
+        x <- theCode.zipWithIndex
+        y <- guess.zipWithIndex
+        if x._2 == y._2 && x._1 != y._1 // _1 stands for value, _2 for index
+      } yield x._1).groupDigits
 
     val misplacedDigitsCount = // I group here the digits in the guess which are not correct
-      (for { x <- theCode.zipWithIndex
-             y <- guess.zipWithIndex
-             if x._2 == y._2 && x._1 != y._1 // _1 stands for value, _2 for index
-       } yield y._1)
-         .groupDigits
-         // the line below... it is so, I believe
-         .map{ case (k,v) => v min groupedCodeRest.getOrElse(k,0) }
-         .sum
+      (for {
+        x <- theCode.zipWithIndex
+        y <- guess.zipWithIndex
+        if x._2 == y._2 && x._1 != y._1 // _1 stands for value, _2 for index
+      } yield y._1).groupDigits
+        // the line below... it is so, I believe
+        .map { case (k, v) => v min groupedCodeRest.getOrElse(k, 0) }.sum
 
     // FIXME it should be a case class!!! I didn't know 5 years ago, sorry!
-    Map( "correct"   -> correctDigits.values.sum
-       , "misplaced" -> misplacedDigitsCount )
+    Map("correct" -> correctDigits.values.sum, "misplaced" -> misplacedDigitsCount)
   }
 }
 
@@ -94,8 +93,9 @@ object MasterMindCLI extends App {
   // at the moment it only parses the arguments list
   // it could contain some restrictions though
   // (Either would be preferred to Option in such case, I guess)
-  def parseArgs(args : Array[String]) : Option[Array[Int]] =
-    try Some(args.map(_.toInt)) catch {case e: Exception => None}
+  def parseArgs(args: Array[String]): Option[Array[Int]] =
+    try Some(args.map(_.toInt))
+    catch { case e: Exception => None }
 
   @tailrec
   def readCode(codeLength: Int, maxDigit: Int, currentTurn: Int, maxTurn: Int): Code = {
@@ -104,9 +104,7 @@ object MasterMindCLI extends App {
 
       val parsedCode =
         try {
-            Right(codeString
-              .trim.split(" +")
-              .toList.map(_.toInt))
+          Right(codeString.trim.split(" +").toList.map(_.toInt))
         } catch {
           case nfe: NumberFormatException => Left("Invalid input.")
         }
@@ -116,14 +114,14 @@ object MasterMindCLI extends App {
 
         case Right(codeSeq) => {
           // check if numbers are in the interval (0,maxDigit]
-          if ( codeSeq.exists(x => x<=0 || x>maxDigit) ) {
+          if (codeSeq.exists(x => x <= 0 || x > maxDigit)) {
             Left(s"One of the digits is not between 1 and $maxDigit.")
           } else
           // check the length (should be equal to codeLength)
-          if ( codeSeq.length != codeLength ) {
+          if (codeSeq.length != codeLength) {
             Left(s"The code length should equal $codeLength.")
           } else
-          parsedCode
+            parsedCode
         }
       }
     }
@@ -132,26 +130,31 @@ object MasterMindCLI extends App {
 
     parseCode(codeString, codeLength, maxDigit) match {
       case Right(code) => code
-      case Left(errorMsg) => println(errorMsg)
-                             readCode(codeLength, maxDigit, currentTurn, maxTurn)
+      case Left(errorMsg) =>
+        println(errorMsg)
+        readCode(codeLength, maxDigit, currentTurn, maxTurn)
     }
   }
 
   @tailrec
-  def controlLoop( generatedCode: Code
-                 , maxTurnNumber: Integer
-                 , codeLength: Integer
-                 , maxDigit: Integer
-                 , currentTurn: Integer ): Unit = {
+  def controlLoop(
+      generatedCode: Code,
+      maxTurnNumber: Integer,
+      codeLength: Integer,
+      maxDigit: Integer,
+      currentTurn: Integer
+  ): Unit = {
 
-    val result = MasterMind.checkGuess( generatedCode, readCode(codeLength,maxDigit
-                                                               ,currentTurn,maxTurnNumber) )
+    val result = MasterMind.checkGuess(
+      generatedCode,
+      readCode(codeLength, maxDigit, currentTurn, maxTurnNumber)
+    )
 
-    if ( currentTurn == maxTurnNumber )
+    if (currentTurn == maxTurnNumber)
       println(defaults.loseMessage)
-    else if ( result("correct") != codeLength ) {
+    else if (result("correct") != codeLength) {
       println(s"Result: ${result("correct")} correct, ${result("misplaced")} misplaced.")
-      controlLoop(generatedCode,maxTurnNumber,codeLength,maxDigit,currentTurn+1)
+      controlLoop(generatedCode, maxTurnNumber, codeLength, maxDigit, currentTurn + 1)
     } else {
       println(defaults.wonMessage)
     }
@@ -164,20 +167,18 @@ object MasterMindCLI extends App {
   // (I didn't have time nor idea to do it better)
   val parsedArgs = parseArgs(args)
 
-  val Array(maxTurnNumber, codeLength, maxDigit) : Array[Int] = {
+  val Array(maxTurnNumber, codeLength, maxDigit): Array[Int] = {
     // I know it's a stupid condition - this is what I was talking about above
-    if ( args.length == 3 ) {
+    if (args.length == 3) {
       parsedArgs
     } else {
       None
     }
-  }.getOrElse( Array( defaults.defMaxTurnNumber
-                    , defaults.defCodeLength
-                    , defaults.defMaxDigit ) )
+  }.getOrElse(Array(defaults.defMaxTurnNumber, defaults.defCodeLength, defaults.defMaxDigit))
 
   /* END OF PREPARATIONS */
   /* HERE WE START THE GAME */
-  val code = MasterMind.generateCode(codeLength,maxDigit)
+  val code = MasterMind.generateCode(codeLength, maxDigit)
 
   // uncomment it if you want to debug (I didn't manage to configure it in a better way, sorry)
   // logger.debug(s"Logger test. The code is ${code.mkString}.")
@@ -190,11 +191,13 @@ object MasterMindCLI extends App {
     println("\n" + defaults.invalidParamsWarning)
 
   // print the game configuration
-  println( "\n"+
-           "Game parameters: \n"+
-          s"  No of turns: $maxTurnNumber\n"+
-          s"  Code length: $codeLength\n"+
-          s"  Available digits: 1 to $maxDigit inclusive\n")
+  println(
+    "\n" +
+      "Game parameters: \n" +
+      s"  No of turns: $maxTurnNumber\n" +
+      s"  Code length: $codeLength\n" +
+      s"  Available digits: 1 to $maxDigit inclusive\n"
+  )
 
   controlLoop(code, maxTurnNumber, codeLength, maxDigit, 1)
 }
--- src/test/scala/CheckTester.scala
+++ src/test/scala/CheckTester.scala
@@ -5,74 +5,62 @@ import org.scalatest.funsuite.AnyFunSuite
 class CheckSpec extends AnyFunSuite {
 
   test("counting correct digits in correct positions") {
-    val testData = ( Seq(1,2,3,4)
-                   , Seq(1,2,3,4)
-                   , (4,0) )
+    val testData = (Seq(1, 2, 3, 4), Seq(1, 2, 3, 4), (4, 0))
 
-    assertResult( testData._3 ) {
+    assertResult(testData._3) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 
   test("counting misplaced digits") {
-    val testData = ( Seq(1,2,3,4)
-                   , Seq(4,1,2,3)
-                   , (0,4) )
+    val testData = (Seq(1, 2, 3, 4), Seq(4, 1, 2, 3), (0, 4))
 
-    assertResult( testData._3 ) {
+    assertResult(testData._3) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 
   test("counting both without counting twice") {
-    val testData = ( Seq(1,2,3,4)
-                   , Seq(1,2,4,2)
-                   , (2,1) ) // there is no place for the second '2'
-                             // so it can't be considered as misplaced
+    val testData =
+      (Seq(1, 2, 3, 4), Seq(1, 2, 4, 2), (2, 1)) // there is no place for the second '2'
+    // so it can't be considered as misplaced
 
     val clue = "// the digit '2' might have been counted twice\n" +
-               "// the code was ("+testData._1.mkString("") + ") " +
-               "and the guess was ("+testData._2.mkString("") + ")"
+      "// the code was (" + testData._1.mkString("") + ") " +
+      "and the guess was (" + testData._2.mkString("") + ")"
 
-    assertResult( testData._3, clue ) {
+    assertResult(testData._3, clue) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 
   test("some other tests: code 222 and guess 222") {
-    val testData = ( Seq(2,2,2,2)
-                   , Seq(2,2,2,2)
-                   , (4,0) )
+    val testData = (Seq(2, 2, 2, 2), Seq(2, 2, 2, 2), (4, 0))
 
-    assertResult( testData._3 ) {
+    assertResult(testData._3) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 
   test("some other tests: code 43453 and guess 12345") {
-    val testData = ( Seq(4,3,4,5,3)
-                   , Seq(1,2,3,4,5)
-                   , (0,3) )
+    val testData = (Seq(4, 3, 4, 5, 3), Seq(1, 2, 3, 4, 5), (0, 3))
 
-    assertResult( testData._3 ) {
+    assertResult(testData._3) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 
   test("some other tests: code 223 and guess 222") {
-    val testData = ( Seq(2,2,3)
-                   , Seq(2,2,2)
-                   , (2,0) )
+    val testData = (Seq(2, 2, 3), Seq(2, 2, 2), (2, 0))
 
-    assertResult( testData._3 ) {
+    assertResult(testData._3) {
       val resultMap = MasterMind.checkGuess(testData._1, testData._2)
-      ( resultMap("correct"), resultMap("misplaced") )
+      (resultMap("correct"), resultMap("misplaced"))
     }
   }
 }
-
